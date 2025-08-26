962bb1cc9cd8bd2173f932a134d7eb6c1b3baa5e
Ehidiamen Agbonrofo <aehidiamen1@gmail.com>
2025-08-26 09:30:18 +0100

    Adding comments to the MainMenuManager

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private bool _debugMode;
+    //Setting the possible values of the main menu buttons
     public enum MainMenuButtons { play, options, howtoplay, quit };
+    // Displaying a message when the code is debugged
     public void MainMenuButtonClicked(MainMenuButtons buttonClicked)
     {
         DebugMessage("Button Clicked" + buttonClicked.ToString());
