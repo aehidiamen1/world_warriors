87c339cc654f45b637efe0b4b068f84a1ff2b07d
Ehidiamen Agbonrofo <aehidiamen1@gmail.com>
2025-08-26 11:34:39 +0100

    Adding code to run when the button is clicked

--- Assets/Scripts/MainMenuButtonManager.cs
+++ Assets/Scripts/MainMenuButtonManager.cs
@@ -3,4 +3,10 @@ using UnityEngine;
 public class MainMenuButtonManager : MonoBehaviour
 {
     [SerializeField] private MainMenuManager.MainMenuButtons _buttonType;
+    //Code to run when the button is clicked.
+    public void ButtonClicked()
+    {
+        MainMenuManager._.MainMenuButtonClicked(_buttonType);
+
+    }
 }
--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -2,9 +2,25 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public static MainMenuManager _;
     [SerializeField] private bool _debugMode;
     //Setting the possible values of the main menu buttons
     public enum MainMenuButtons { play, options, howtoplay, quit };
+
+    // Method that unity calls when the screen starts up
+    public void Awake()
+    {
+        //Checks whether more than one script is trying to set the value of the singleton.       
+        if (_ == null) //Checks if _ hasn't been set to anything
+        {
+            _ = this;
+        }
+        else
+        {
+            Debug.LogError("There are more than one MainMenuManagers in the scene");
+        }
+    }
+
     // Displaying a message when the code is debugged
     public void MainMenuButtonClicked(MainMenuButtons buttonClicked)
     {
