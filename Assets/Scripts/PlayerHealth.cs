1ffacff995bb13b4b52afdc84df384754c575fe1
Ehidiamen Agbonrofo <aehidiamen1@gmail.com>
2025-11-09 13:38:47 +0000

    Preventing the health to drop below zero

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class PlayerHealth : MonoBehaviour
 {
     public float health;
+    public float minHealth;
     public float maxHealth;
     public Image healthBar;
 
@@ -11,11 +12,15 @@ public class PlayerHealth : MonoBehaviour
     void Start()
     {
         maxHealth = health;
+        //Ensures that the starting health is within the set boundaries
+        health = Mathf.Clamp(health, minHealth, maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Restrict the health to min and max health
+        health = Mathf.Clamp(health, minHealth, maxHealth);
         //Restrict the fillAmount to the min health and max health
         healthBar.fillAmount = Mathf.Clamp(health / maxHealth, 0 ,1);
     }
