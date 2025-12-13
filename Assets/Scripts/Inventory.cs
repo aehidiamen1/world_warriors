05d98a024fc0568a1b7b528738226b5712891904
Ehidiamen Agbonrofo <aehidiamen1@gmail.com>
2025-12-13 12:49:58 +0000

    trying to fix error with inventory not showing up

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -2,6 +2,53 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    public bool[] isFull;
-    public GameObject[] slots;
+    public static Inventory inventory;
+    public static bool[] isFull;
+    public static GameObject[] slots;
+    public GameObject inventoryPanel;
+    private static GameObject persistentInventoryPanel;
+    public GameObject[] slotsSetup;
+
+    void Awake()
+    {
+        if (inventory == null)
+        {
+            inventory = this;
+
+            if (slots == null || slots.Length == 0)
+            {
+                slots = slotsSetup;
+            }
+
+            if (isFull == null || isFull.Length == 0)
+            {
+                isFull = new bool[slots.Length];
+            }
+
+            if (inventoryPanel != null && persistentInventoryPanel == null)
+            {
+                DontDestroyOnLoad(inventoryPanel);
+                persistentInventoryPanel = inventoryPanel;
+            }
+        }
+        else
+        {
+            inventory = this;
+
+            if (inventoryPanel != null && persistentInventoryPanel != null)
+            {
+                Destroy(inventoryPanel);
+            }
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Make sure our arrays are the right size
+        if (isFull == null || isFull.Length == 0)
+        {
+            isFull = new bool[slots.Length];
+        }
+    }
 }
--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -2,27 +2,21 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
-    private Inventory inventory;
     public GameObject itemButton;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
-    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             //Checking if the item can be added to the inventory
-            for (int i = 0; i < inventory.slots.Length; i++)
+            for (int i = 0; i < Inventory.slots.Length; i++)
             {
-                if (inventory.isFull[i] == false)
+                // Check if this slot is empty
+                if (Inventory.isFull[i] == false)
                 {
-                    //Add item to inventory
-                    inventory.isFull[i] = true;
-                    Instantiate(itemButton, inventory.slots[i].transform, false);
+                    // Add item to the inventory
+                    Inventory.isFull[i] = true;
+                    Instantiate(itemButton, Inventory.slots[i].transform, false);
                     Destroy(gameObject);
                     break;
                 }
