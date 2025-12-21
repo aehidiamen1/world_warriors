9d2be8f5a0e42460ea3b780e3a0be5b4bfa1fddf
Ehidiamen Agbonrofo <aehidiamen1@gmail.com>
2025-12-21 19:24:18 +0000

    Resetting coin count when a new game starts attempt 2

--- Assets/Scripts/ResetGameValues.cs
+++ Assets/Scripts/ResetGameValues.cs
@@ -5,7 +5,7 @@ public class ResetGameValues : MonoBehaviour
     [SerializeField]
     private FloatSO coinCountSO;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void Start()
+    private void Awake()
     {
         coinCountSO.Value = 0;
     }
