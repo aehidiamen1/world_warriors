7d454a924084cd261303e342c4a221b2e2b1aec2
Ehidiamen Agbonrofo <aehidiamen1@gmail.com>
2025-12-22 19:55:40 +0000

    carrying over inventory values attemp 4

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
     private Inventory inventory;
-    public GameObject itemButton;
     public ItemData itemData;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
