5024e0c74ed122ffbab99427726c213278517eb6
Ehidiamen Agbonrofo <aehidiamen1@gmail.com>
2025-12-22 15:51:51 +0000

    carrying over inventory values attemp 4 - error where the inventory values were being reset constantly

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -3,17 +3,24 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
     [SerializeField]
-    public ArraySO isFull;
+    private ArraySO isFull;
     [SerializeField]
     public GameObject[] slots;
+    private bool[] runtimeValues;
+
+    private void Start()
+    {
+        // Create a runtime copy to use during Play Mode
+        runtimeValues = (bool[])isFull.Value.Clone();
+    }
 
     public bool IsSlotFull(int index)
     {
-        return isFull.Value[index];
+        return runtimeValues[index];
     }
 
     public void SetSlotFull(int index, bool value)
     {
-        isFull.Value[index] = value;
+        runtimeValues[index] = value;
     }
 }
