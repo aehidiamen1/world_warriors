b5886efc9f931bfe4960b60627f126eb2aac310a
Ehidiamen Agbonrofo <aehidiamen1@gmail.com>
2025-12-28 14:48:04 +0000

    Adding a camera switcher for the boss fight - attempt 1

---
+++ Assets/Scripts/CameraBoundsSwitcher.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class CameraBoundsSwitcher : MonoBehaviour
+{
+    [SerializeField] private CinemachineCamera virtualCamera;
+    [SerializeField] private BoxCollider2D newBoundsCollider;
+    
+    private CinemachineConfiner2D confiner;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        confiner = virtualCamera.GetComponent<CinemachineConfiner2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Switch to new bounds
+            confiner.BoundingShape2D = newBoundsCollider;
+            confiner.InvalidateBoundingShapeCache();
+        }
+    }
+}
