40a32d1877cfab4c980ccdd8648ec22f87cfe915
Ehidiamen Agbonrofo <aehidiamen1@gmail.com>
2025-12-29 16:30:06 +0000

    Adding a trigger for the boss to load - attempt 1

---
+++ Assets/Scripts/BossTrigger.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class BossTrigger : MonoBehaviour
+{
+    public Animator bossAnimator;
+    public MonoBehaviour bossScript;
+
+    private bool activated = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (activated)
+        {
+            return;
+        }
+
+        if (collision.GetComponent<PlayerMovement>() != null)
+        {
+            ActivateBoss();
+        }
+    }
+
+    void ActivateBoss()
+    {
+        activated = true;
+
+        if (bossAnimator != null)
+        {
+            bossAnimator.enabled = true;
+        }
+
+        if (bossScript != null)
+        {
+            Debug.Log("Boss activated!");
+            
+            bossScript.enabled = true;
+        }
+
+        Destroy(gameObject);
+    }
+}
