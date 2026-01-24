d0c51d61d013877b823d9b2094abdbd865d63a90
Ehidiamen Agbonrofo <aehidiamen1@gmail.com>
2026-01-24 13:48:12 +0000

    Adding a fade in for the game over screen - attempt 2 combining it with the game over logic

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -6,9 +6,18 @@ public class GameOverScreen : MonoBehaviour
     public GameObject gameOverUI;
     public static bool reset = false;
 
+    public CanvasGroupFader fader;
+
     public void GameOver()
     {
         gameOverUI.SetActive(true);
+
+        // Fade in the game over screen
+        if (fader != null)
+        {
+            fader.Fade(true);
+        }
+
         //Pause the game
         Time.timeScale = 0f;
     }
