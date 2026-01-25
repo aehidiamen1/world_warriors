49d5290491093f7801d795ac25af4ea207bbbfb7
Ehidiamen Agbonrofo <aehidiamen1@gmail.com>
2026-01-25 15:04:43 +0000

    Adding a code to the buy button in shop - preventing player from buying if they dont have enough money

--- Assets/Scripts/BuyItem.cs
+++ Assets/Scripts/BuyItem.cs
@@ -7,22 +7,41 @@ public class BuyItem : MonoBehaviour
     [SerializeField] float itemCost;
     [SerializeField] private FloatSO coinCountSO;
 
+    bool canPurchase = false;
+
     public void PurchaseItem()
     {
-        Inventory inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
-        
-        // Store the item in the first available slot
-        for (int i = 0; i < inventory.slots.Length; i++)
+        //Check if the player has enough coins to purchase the item
+        if (coinCountSO.Value >= itemCost)
         {
-            if (inventory.inventoryData.isFull[i] == false)
-            {
-                inventory.inventoryData.StoreItem(i, itemButton);
-                Instantiate(itemButton, inventory.slots[i].transform, false);
-                break;
-            }
+            canPurchase = true;
+        }
+        else if (coinCountSO.Value < itemCost)
+        {
+            // Player doesn't have enough coins so exit the function
+            Debug.Log("Not enough coins to purchase this item.");
+            canPurchase = false;
+            return;
         }
 
-        // Deduct the cost from the player's coins
-        coinCountSO.Value -= itemCost;
+        // If the player can purchase the item, add it to their inventory
+        if (canPurchase)
+        {
+            Inventory inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+            
+            // Store the item in the first available slot
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.inventoryData.isFull[i] == false)
+                {
+                    inventory.inventoryData.StoreItem(i, itemButton);
+                    Instantiate(itemButton, inventory.slots[i].transform, false);
+                    break;
+                }
+            }
+
+            // Deduct the cost from the player's coins
+            coinCountSO.Value -= itemCost;
+        }
     }
 }
