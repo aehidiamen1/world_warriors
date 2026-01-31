166e4e25db4f5d595d061b2549f12636a1834e37
Ehidiamen Agbonrofo <aehidiamen1@gmail.com>
2026-01-31 12:36:15 +0000

    Adding a win screen

---
+++ Assets/Scripts/WinScreen.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreen : MonoBehaviour
+{
+    public static bool reset = false;
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+        reset = true;
+        Time.timeScale = 1f;
+    }
+}
